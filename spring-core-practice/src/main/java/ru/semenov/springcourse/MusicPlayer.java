cdff864c58011684108637fdd60e21227adb6328
cepehao <cepeh@DESKTOP-N3BI952>
2023-08-03 04:43:19 +0500

    Lesson 7. Dependency Injection part II

--- spring-core-practice/src/main/java/ru/semenov/springcourse/MusicPlayer.java
+++ spring-core-practice/src/main/java/ru/semenov/springcourse/MusicPlayer.java
@@ -1,14 +1,43 @@
 package ru.semenov.springcourse;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+
+    private List<Music> musicList = new ArrayList<>();
+
+    private String name;
+    private int volume;
+
+
+    public MusicPlayer() {
+
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 
     void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music: musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
