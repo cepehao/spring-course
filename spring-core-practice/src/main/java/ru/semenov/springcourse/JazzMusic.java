85ab0c004cd45d0f475ec6b9a2d3081361cac19c
cepehao <cepeh@DESKTOP-N3BI952>
2023-08-23 20:22:41 +0500

    Lesson 12. Annotation @Qualifier, Dependency Injection

--- spring-core-practice/src/main/java/ru/semenov/springcourse/JazzMusic.java
+++ spring-core-practice/src/main/java/ru/semenov/springcourse/JazzMusic.java
@@ -2,6 +2,7 @@ package ru.semenov.springcourse;
 
 import org.springframework.stereotype.Component;
 
+@Component
 public class JazzMusic implements Music {
     @Override
     public String getSong() {
