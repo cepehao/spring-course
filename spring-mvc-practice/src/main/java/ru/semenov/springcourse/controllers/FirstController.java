83cadf807d51a9f6d22b53c5085f3b1d648fcb38
cepehao <cepeh@DESKTOP-N3BI952>
2023-08-27 18:08:28 +0500

    Lesson 20. GET request parameters. Annotation @RequestParam.

--- spring-mvc-practice/src/main/java/ru/semenov/springcourse/controllers/FirstController.java
+++ spring-mvc-practice/src/main/java/ru/semenov/springcourse/controllers/FirstController.java
@@ -2,12 +2,19 @@ package ru.semenov.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello from " + name + ' ' + surname);
+
         return "first/hello";
     }
 
