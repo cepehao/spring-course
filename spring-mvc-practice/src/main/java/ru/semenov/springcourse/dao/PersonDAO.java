d6e8620bf18ba6c224c1c08a6ce1553f4caf4c10
SemenovSP <rpadvans@yandex.ru>
2023-08-30 03:04:39 +0500

    Lesson 22. First CRUD application without DB, pattern DAO

---
+++ spring-mvc-practice/src/main/java/ru/semenov/springcourse/dao/PersonDAO.java
@@ -0,0 +1,31 @@
+package ru.semenov.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.semenov.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT = 0;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Danil"));
+        people.add(new Person(++PEOPLE_COUNT, "Sergey"));
+        people.add(new Person(++PEOPLE_COUNT, "Ivan"));
+        people.add(new Person(++PEOPLE_COUNT, "Alexey"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
+    }
+}
