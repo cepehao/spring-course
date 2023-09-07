29910da1ca048cd8787b32e2a63d44909f68b57c
SemenovSP <rpadvans@yandex.ru>
2023-09-07 20:19:56 +0500

    Lesson 26. JDBC API, add db to CRUD app

--- spring-mvc-practice/src/main/java/ru/semenov/springcourse/dao/PersonDAO.java
+++ spring-mvc-practice/src/main/java/ru/semenov/springcourse/dao/PersonDAO.java
@@ -3,6 +3,7 @@ package ru.semenov.springcourse.dao;
 import org.springframework.stereotype.Component;
 import ru.semenov.springcourse.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,29 +11,72 @@ import java.util.List;
 public class PersonDAO {
 
     private static int PEOPLE_COUNT = 0;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>();
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
 
-        people.add(new Person(++PEOPLE_COUNT, "Danil", 22, "danil@yandex.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Sergey", 25, "Seregea228@yandex.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Ivan", 33, "helloworld@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Alexey", 5, "alex@gmail.com"));
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while(resultSet.next()) {
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
+//        return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
 
-        people.add(person);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() +
+                    "'," + person.getAge() + ",'" + person.getEmail() + "')";
+            statement.executeQuery(SQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
     }
 
     public void update(Person newPerson, int id) {
@@ -44,6 +88,6 @@ public class PersonDAO {
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+//        people.removeIf(p -> p.getId() == id);
     }
 }
