ed0654db1ed4a6b2f97be4a30eee168355c81e87
SemenovSP <rpadvans@yandex.ru>
2024-01-15 00:50:57 +0500

    Lesson 43. Pattern Validation

--- spring-mvc-practice/src/main/java/ru/semenov/springcourse/dao/PersonDAO.java
+++ spring-mvc-practice/src/main/java/ru/semenov/springcourse/dao/PersonDAO.java
@@ -37,13 +37,13 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person (name, age, email) VALUES (?, ?, ?)",
-                person.getName(), person.getAge(), person.getEmail());
+        jdbcTemplate.update("INSERT INTO Person (name, age, email, address) VALUES (?, ?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail(), person.getAddress());
     }
 
     public void update(Person updatedPerson, int id) {
-        jdbcTemplate.update("UPDATE Person SET name = ?, age = ?, email = ? WHERE id = ?",
-                updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
+        jdbcTemplate.update("UPDATE Person SET name = ?, age = ?, email = ?, address = ? WHERE id = ?",
+                updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), updatedPerson.getAddress(), id);
     }
 
     public void delete(int id) {
